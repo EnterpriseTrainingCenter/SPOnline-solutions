8995d6c35d9fa307d31ddf94e7d87fefdbb36f18
Roman Korecky <Roman.Korecky@easyon.at>
2024-08-19 12:05:48 +0200

    Adds script to install vscode

---
+++ LabResources/Solutions/Install-VSCode.ps1
@@ -0,0 +1,28 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [switch]
+    $SkipDependencies
+)
+
+#region Prerequisites
+
+if (-not $SkipDependencies) {
+    . (Join-Path -Path $PSScriptRoot -ChildPath 'Invoke-Dependencies.ps1') `
+        -Script $MyInvocation.MyCommand `
+        -Confirm:$false
+}
+
+#endregion Prerequisites
+
+#region Practice: Install Visual Studio Code
+
+Write-Host 'Practice: Install Visual Studio Code'
+
+if (-not (Get-Package -Name 'Microsoft Visual Studio Code (User)')) {
+    Write-Verbose '            Download and install Microsoft Visual Studio Code'
+    winget install --id XP9KHM4BK9FZ7Q --accept-package-agreements --accept-source-agreements --force
+}
+
+
+#endregion Practice: Install Visual Studio Code
