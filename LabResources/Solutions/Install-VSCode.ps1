4c24a0289a880cc9347fbbc8140c4f5cabfbaaa6
Roman Korecky <Roman.Korecky@easyon.at>
2024-08-19 13:00:44 +0200

    Fixes compatibility with PS Core

--- LabResources/Solutions/Install-VSCode.ps1
+++ LabResources/Solutions/Install-VSCode.ps1
@@ -19,10 +19,15 @@ if (-not $SkipDependencies) {
 
 Write-Host 'Practice: Install Visual Studio Code'
 
-if (-not (Get-Package -Name 'Microsoft Visual Studio Code (User)')) {
+if (-not (
+    Get-ItemProperty `
+        'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' |
+    Where-Object { 
+        $PSItem.DisplayName -eq 'Microsoft Visual Studio Code (User)' 
+    }
+)) {
     Write-Verbose '            Download and install Microsoft Visual Studio Code'
     winget install --id XP9KHM4BK9FZ7Q --accept-package-agreements --accept-source-agreements --force
 }
-
 
 #endregion Practice: Install Visual Studio Code
