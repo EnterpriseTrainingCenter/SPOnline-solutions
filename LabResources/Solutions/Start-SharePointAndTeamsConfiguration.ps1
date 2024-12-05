e052ec4715580763df397b3cfaac5cee63855450
Roman Korecky <Roman.Korecky@easyon.at>
2024-12-05 18:57:45 +0100

    Increases performance

--- LabResources/Solutions/Start-SharePointAndTeamsConfiguration.ps1
+++ LabResources/Solutions/Start-SharePointAndTeamsConfiguration.ps1
@@ -208,11 +208,11 @@ powershell.exe -File $file $verbose
 
 #region Task 5: Verify the functionality of the PowerShell modules
 
-Write-Host '        Task 5: Verify the functionality of the PowerShell modules'
+# Write-Host '        Task 5: Verify the functionality of the PowerShell modules'
 
-Write-Verbose 'Import the modules'
-Import-Module Microsoft.Graph
-Import-Module MicrosoftTeams # Always last
+# Write-Verbose 'Import the modules'
+# Import-Module Microsoft.Graph
+# Import-Module MicrosoftTeams # Always last
 
 #endregion Exercise 1: Get started with PowerShell
 
